a3edb1a5d95354bdc9f9f11b346a52cfdebdaec7
ibrahim Lahlou <ibrahim.lahlou021@gmail.com>
2023-09-02 13:53:25 +0100

    some issues about settting up python 3.6 for the model

--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as f:
-        long_description = f.read()
+    long_description = f.read()
 
-__version__ = "0.0.1"
+
+__version__ = "0.0.0"
 
 REPO_NAME = "spellX"
-AUTHOR_USER_NAME = "IbLahlou"
-SRC_REPO="spellX"
+AUTHOR_USER_NAME = "IBLahlou"
+SRC_REPO = "spellX"
 AUTHOR_EMAIL = "ibrahimlahlou021@gmail.com"
 
+
+
 setuptools.setup(
-    name=REPO_NAME,
+    name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small example package",
+    description="A small python package for NLP app",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
-
-
