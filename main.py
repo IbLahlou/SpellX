ad92061e917b66126763aa3c00c46bd4996da97f
ibrahim Lahlou <ibrahim.lahlou021@gmail.com>
2023-09-05 14:49:56 +0100

    Data Validation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from spellX import logger
 from spellX.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from spellX.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -12,3 +12,13 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+         logger.exception(e)
+         raise e
