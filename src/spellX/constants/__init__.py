6fa9336f4bf6209ad9dc3c1ce2fa763b47f1f738
Lahlou ibrahim <105231126+IbLahlou@users.noreply.github.com>
2023-09-06 15:53:16 +0100

    tokinizer research

--- src/spellX/constants/__init__.py
+++ src/spellX/constants/__init__.py
@@ -2,3 +2,7 @@ from pathlib import Path
 
 CONFIG_FILE_PATH = Path("./config/config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
+SOS = '\t'  # Début de séquence.
+EOS = '*'   # Fin de séquence.# Liste des caractères utilisés dans le traitement des données texte.
+CHARS = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ')# Expression régulière pour supprimer certains caractères indésirables des données.
+REMOVE_CHARS = '[#$%"\+@<=>!&,-.?:;()*\[\]^_`{|}~/\d\t\n\r\x0b\x0c]'
